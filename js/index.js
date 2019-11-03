ac231969a3858f9a3589946c211e4e0311bea3f8
René Pacios <rene15009@hotmail.com>
2019-11-03 04:40:52 +0100

    added konami-code

---
+++ js/index.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../cdn/js/cheet.min.js" />
+; (function (cnx, doc, undefined) {
+
+
+    var loadjs = function (scriptPath, onLoadCallBack) {
+
+        var script = doc.createElement("script");
+        script.type = "text/javascript";
+        script.src = scriptPath;
+        !!onLoadCallBack && (script.onload = onLoadCallBack);
+
+        doc.body.appendChild(script);
+    };
+
+    loadjs("/cdn/harlem/harlem-shake.min.js");
+    loadjs("/cdn/js/cheet.min.js", function () {
+        //console.log(window, window.cnx,cheet);
+        cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+            cnx.harlem.start();
+        });
+    });
+
+
+})(window.cnx = window.cnx || {}, document);
